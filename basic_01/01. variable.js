f53e89d476d5775b4b7a09adfaf95c8af0a33366
Adi <adityakumaryadav2598@gmail.com>
2025-11-25 23:25:38 +0530

    Add files via upload

---
+++ basic_01/01. variable.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "adityakunmar@gmail"
+var accountPassword = "1234"
+accountcity = "bhopal"
+let accountstate;
+
+// accountId = 2
+accountEmail = "adi@gmail.com"
+accountPassword = "13451235"
+accountcity = "chapra"
+
+/*
+not prefer to use var due to block scrope and function scope
+*/
+
+
+console.log(accountId);
+console.table([accountEmail, accountId, accountPassword,accountcity, accountstate])
