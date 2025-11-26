cb1a742fa79b57e4e221dd0f73b6d4e2807f931c
Adi <adityakumaryadav2598@gmail.com>
2025-11-26 23:48:29 +0530

    Add files via upload

--- basic_01/02. datatype.js
+++ basic_01/02. datatype.js
@@ -1,1 +1,21 @@
- 
+ "use strict";// treat all js code as newer version
+
+//  alert(3 + 3) // we are using nodejs, not  brower
+
+console.log(3 + 3)
+console.log("adi")
+
+let name = "adi"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+//null => standalone value
+//undefined =>
+// symbol => unique
+
+console.log(typeof undefined)
