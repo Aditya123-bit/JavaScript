9fba33152377c7516ccf671bbf4c87c498f41492
Aditya123-bit <adityakumaryadav2598@gmail.com>
2025-12-02 23:37:38 +0530

    conversion of datatype

---
+++ Basic_01/03.dataconversion.js
@@ -0,0 +1,28 @@
+let score = "aditya"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//other variable to Number
+// "33" => 33
+// "33abc" => NaN
+// true => 1 or false => 0
+
+let isLoggedIn = "aditya"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true or 0 => false
+// "" => false
+// "aditya" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
